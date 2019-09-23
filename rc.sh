3c13f6fe671b8ccfff6770b0316eac352c65245b
Nikola <the.final.frontend@gmail.com>
2019-09-23 14:00:46 +0200

    Update rc.sh

--- rc.sh
+++ rc.sh
@@ -1,14 +1,24 @@
-# aliases for rc
+# RC aliases 
 
-alias l='exa -bgl --group-directories-first --git --sort name'	      # 'ls' implementation for all files
-alias ll='exa -GFmxl --group-directories-first --sort type'				    # 'ls' implementation for all files
-alias lll='exa -lGxFmha --group-directories-first --git'              # 'ls' implementation for all files
+# 'ls' implementation for all files
+alias l='exa -bgl --group-directories-first --git --sort name'
+# 'ls' implementation for all files
+alias ll='exa -GFmxl --group-directories-first --sort type'				    
+# 'ls' implementation for all files
+alias lll='exa -lGxFmha --group-directories-first --git'              
 
-alias ..='cd ..'				                                              # Go back 1 directory level
-alias ...='cd ../../'				                                          # Go back 2 directory level
+# Go back 1 directory level
+alias ..='cd ..'				                                              
+# Go back 2 directory level
+alias ...='cd ../../'				                                          
 
-alias o='open -a Finder ./'			                                      # Opens current directory in MacOS Finder
-alias ~="cd ~"					                                              # Home
-alias c='clear'                        		                            # Clear terminal display
+# Opens current directory in MacOS Finder
+alias o='open -a Finder ./'			                                      
+# Home
+alias ~="cd ~"					                                              
 
-alias qf="find . -name " 			                                        # Quickly search for file
+# Clear terminal display
+alias c='clear'                        		                            
+
+# Quickly search for file
+alias qf="find . -name " 			                                        
